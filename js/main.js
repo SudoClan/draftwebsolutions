6e0b827fcc7d4f7db5b78066de38aeaac047c14a
saifdevops <saifulmalook1@hotmail.com>
2017-12-14 03:18:09 +0000

    removed(commented) the color slider on side page, color switcher

--- js/main.js
+++ js/main.js
@@ -15,14 +15,14 @@ Author : themelooks.com
      * COMMON VARIABLES
      * ------------------------------------------------------------------------- */
     var $wn = $(window);
-    
+
     $(document).ready(function () {
         /* ------------------------------------------------------------------------- *
          * BACKGROUND IMAGE
          * ------------------------------------------------------------------------- */
         var $bgImg = $('[data-bg-img]'),
             $bgImgT;
-        
+
         $bgImg.each(function () {
             $bgImgT = $(this);
             $bgImgT.css('background-image', 'url('+ $bgImgT.data('bg-img') +')').removeAttr('data-bg-img').addClass('bg--img');
@@ -40,38 +40,38 @@ Author : themelooks.com
             if( $t.find(".btn--ripple-ink").length === 0) {
                 $t.prepend("<span class='btn--ripple-ink'></span>");
             }
-                 
+
             $btnRippleInk = $t.find(".btn--ripple-ink");
             $btnRippleInk.removeClass("btn--ripple-animate");
-             
+
             if( !$btnRippleInk.height() && !$btnRippleInk.width() ) {
                 btnRippleH = Math.max($t.outerWidth(), $t.outerHeight());
                 $btnRippleInk.css({height: btnRippleH, width: btnRippleH});
             }
-             
+
             btnRippleX = e.pageX - $t.offset().left - $btnRippleInk.width() / 2;
             btnRippleY = e.pageY - $t.offset().top - $btnRippleInk.height() / 2;
-             
+
             $btnRippleInk.css({top: btnRippleY + 'px', left: btnRippleX + 'px'}).addClass("btn--ripple-animate");
         });
-        
+
         /* -------------------------------------------------------------------------*
          * COUNTER UP
          * -------------------------------------------------------------------------*/
         var $counterUP = $('.CounterUP');
-            
+
         if ( $counterUP.length ) {
             $counterUP.counterUp({
                 delay: 10,
                 time: 1000
             });
         }
-        
+
         /* ------------------------------------------------------------------------- *
          * FORM VALIDATION
          * ------------------------------------------------------------------------- */
         var $domainSearchForm = $('.domain-search--form form');
-        
+
         if ( $domainSearchForm.length ) {
             $domainSearchForm.validate({
                 rules: {
@@ -84,7 +84,7 @@ Author : themelooks.com
         }
 
         var $subscribeForm = $('.subscribe--form form');
-        
+
         if ( $subscribeForm.length ) {
             $subscribeForm.validate({
                 rules: {
@@ -100,7 +100,7 @@ Author : themelooks.com
         }
 
         var $loginForm = $('.login--form form');
-        
+
         if ( $loginForm.length ) {
             $loginForm.validate({
                 rules: {
@@ -117,7 +117,7 @@ Author : themelooks.com
         }
 
         var $signupForm = $('.signup--form form');
-        
+
         if ( $signupForm.length ) {
             $signupForm.validate({
                 rules: {
@@ -140,7 +140,7 @@ Author : themelooks.com
         }
 
         var $f0fSearchBar = $('.f0f--search-bar form');
-        
+
         if ( $f0fSearchBar.length ) {
             $f0fSearchBar.validate({
                 rules: {
@@ -154,7 +154,7 @@ Author : themelooks.com
 
         var $contactForm = $('.contact--form form'),
             $contactFormStatus = $('.contact--form-status');
-        
+
         if ( $contactForm.length ) {
             $contactForm.validate({
                 rules: {
@@ -185,7 +185,7 @@ Author : themelooks.com
         }
 
         var $blogSearchWidgetForm = $('.blog--search-widget form');
-        
+
         if ( $blogSearchWidgetForm.length ) {
             $blogSearchWidgetForm.validate({
                 rules: {
@@ -198,7 +198,7 @@ Author : themelooks.com
         }
 
         var $blogPostCommentForm = $('.blog--post-comment-form-group form');
-        
+
         if ( $blogPostCommentForm.length ) {
             $blogPostCommentForm.validate({
                 rules: {
@@ -214,36 +214,36 @@ Author : themelooks.com
                 }
             });
         }
-        
+
         /* ------------------------------------------------------------------------- *
          * TOGGLE CLASS ON SCROLL
          * ------------------------------------------------------------------------- */
         var tClass = function (pos, $el, value) {
             return $wn.scrollTop() > pos ? $el.addClass(value) : $el.removeClass(value);
         };
-        
+
         /* ------------------------------------------------------------------------- *
          * HEADER STICKY
          * ------------------------------------------------------------------------- */
         var $hEl = $('#header');
-        
+
         tClass(0, $hEl, 'sticky');
-        
+
         /* ------------------------------------------------------------------------- *
          * BACK TO TOP BUTTON SHOW
          * ------------------------------------------------------------------------- */
         var $backToTopBtn = $('#backToTop');
-        
+
         $backToTopBtn.on('click', function () {
             $('html, body').animate({
                 scrollTop: 0
             }, 800);
-            
+
             return false;
         });
 
         tClass(0, $backToTopBtn, 'show');
-        
+
         /* ------------------------------------------------------------------------- *
          * SCROLL EVENTS
          * ------------------------------------------------------------------------- */
@@ -254,7 +254,7 @@ Author : themelooks.com
             /* BACK TO TOP SHOW */
             tClass(0, $backToTopBtn, 'show'); // TOGGLE CLASS ON SCROLL
         });
-        
+
         /* ------------------------------------------------------------------------- *
          * BANNER
          * ------------------------------------------------------------------------- */
@@ -273,7 +273,7 @@ Author : themelooks.com
                 controls: false
             });
         }
-        
+
         /* ------------------------------------------------------------------------- *
          * DOMAIN EXTENSION
          * ------------------------------------------------------------------------- */
@@ -300,19 +300,19 @@ Author : themelooks.com
                 $fdTr = $fdNavTab.children('.feedback--triangle'),
                 fdP = ($fdNavTabAL.position().left + ($fdNavTabLW / 2)) - 25,
                 $fdT;
-        
+
             /* ON LOAD POSITION THE TRIANGLE */
             $fdTr.css('left', fdP);
-            
+
             /* ON CLICK POSITION THE TRIANGLE */
             $fdNavTabL.on('click', function () {
                 $fdT = $(this);
                 fdP = ($fdT.position().left + ($fdNavTabLW / 2)) - 25;
-                
+
                 $fdTr.css('left', fdP);
             });
         }
-        
+
         /* -------------------------------------------------------------------------*
          * CLIENTS SLIDER
          * -------------------------------------------------------------------------*/
@@ -324,7 +324,7 @@ Author : themelooks.com
                 autoPlay: true
             });
         }
-        
+
         /* -------------------------------------------------------------------------*
          * PRICE DETAILS
          * -------------------------------------------------------------------------*/
@@ -359,7 +359,7 @@ Author : themelooks.com
          * MAP
          * -------------------------------------------------------------------------*/
         var $map = $('#map');
-            
+
         if ( $map.length ) {
             var myLatLng = {
                     lat: $map.data('latitude'),
@@ -369,7 +369,7 @@ Author : themelooks.com
                 map, marker, style;
 
             style = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
-            
+
             map = new google.maps.Map(document.getElementById('map'), {
                 center: myLatLng,
                 zoom: zoomV,
@@ -379,7 +379,7 @@ Author : themelooks.com
                 draggable: false,
                 styles: style
             });
-            
+
             marker = new google.maps.Marker({
                 position: myLatLng,
                 map: map,
@@ -397,48 +397,48 @@ Author : themelooks.com
                 screenName: $twitterTimeline.data('user-name')
             }, document.getElementById('twitterTimeline'));
         }
-        
-        /* ------------------------------------------------------------------------- *
-         * COLOR SWITCHER
-         * ------------------------------------------------------------------------- */
-        if ( $wn.outerWidth() > 767 ) {
-            $.cColorSwitcher({
-                'switcherTitle': 'Main Colors:',
-                'switcherColors': [{
-                    bgColor: '#ff4719',
-                    filepath: 'css/colors/theme-color-1.css'
-                }, {
-                    bgColor: '#8bc34a',
-                    filepath: 'css/colors/theme-color-2.css'
-                }, {
-                    bgColor: '#03a9f4',
-                    filepath: 'css/colors/theme-color-3.css'
-                }, {
-                    bgColor: '#ff9600',
-                    filepath: 'css/colors/theme-color-4.css'
-                }, {
-                    bgColor: '#e91e63',
-                    filepath: 'css/colors/theme-color-5.css'
-                }, {
-                    bgColor: '#00BCD4',
-                    filepath: 'css/colors/theme-color-6.css'
-                }, {
-                    bgColor: '#8CBEB2',
-                    filepath: 'css/colors/theme-color-7.css'
-                }, {
-                    bgColor: '#00B249',
-                    filepath: 'css/colors/theme-color-8.css'
-                }, {
-                    bgColor: '#4267b2',
-                    filepath: 'css/colors/theme-color-9.css'
-                }, {
-                    bgColor: '#e9e9e9',
-                    filepath: 'css/colors/theme-color-10.css'
-                }],
-                'switcherTarget': $('#changeColorScheme')
-            });
-        }
-    });
+
+    //     /* ------------------------------------------------------------------------- *
+    //      * COLOR SWITCHER
+    //      * ------------------------------------------------------------------------- */
+    //     if ( $wn.outerWidth() > 767 ) {
+    //         $.cColorSwitcher({
+    //             'switcherTitle': 'Main Colors:',
+    //             'switcherColors': [{
+    //                 bgColor: '#ff4719',
+    //                 filepath: 'css/colors/theme-color-1.css'
+    //             }, {
+    //                 bgColor: '#8bc34a',
+    //                 filepath: 'css/colors/theme-color-2.css'
+    //             }, {
+    //                 bgColor: '#03a9f4',
+    //                 filepath: 'css/colors/theme-color-3.css'
+    //             }, {
+    //                 bgColor: '#ff9600',
+    //                 filepath: 'css/colors/theme-color-4.css'
+    //             }, {
+    //                 bgColor: '#e91e63',
+    //                 filepath: 'css/colors/theme-color-5.css'
+    //             }, {
+    //                 bgColor: '#00BCD4',
+    //                 filepath: 'css/colors/theme-color-6.css'
+    //             }, {
+    //                 bgColor: '#8CBEB2',
+    //                 filepath: 'css/colors/theme-color-7.css'
+    //             }, {
+    //                 bgColor: '#00B249',
+    //                 filepath: 'css/colors/theme-color-8.css'
+    //             }, {
+    //                 bgColor: '#4267b2',
+    //                 filepath: 'css/colors/theme-color-9.css'
+    //             }, {
+    //                 bgColor: '#e9e9e9',
+    //                 filepath: 'css/colors/theme-color-10.css'
+    //             }],
+    //             'switcherTarget': $('#changeColorScheme')
+    //         });
+    //     }
+    // });
 
     $wn.on('load', function () {
         /* -------------------------------------------------------------------------*
