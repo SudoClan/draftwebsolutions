2f55792a3d9fb0e56f87ff27112b0fe516d92be1
saifdevops <saifulmalook1@hotmail.com>
2017-12-14 03:32:06 +0000

    nav fix

--- includes/footer.php
+++ includes/footer.php
@@ -101,7 +101,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <script>
-by removing this code and using the custom css in the header has fixed this issue.
  $(document).ready(function(){
    $('.dropdown-submenu a.test').on("click", function(e){
      $(this).next('ul').toggle();
