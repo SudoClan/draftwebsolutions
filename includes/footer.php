0e89c406ddad9fe995ab1bb0a317c8d9902f81ac
saifdevops <saifulmalook1@hotmail.com>
2017-12-14 02:21:58 +0000

    NAV BAR FIXED, Sorry i did in this branch but i didnt want to forget now it works on hover but needs fixing or media query for mobile view

--- includes/footer.php
+++ includes/footer.php
@@ -101,15 +101,16 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <script>
- $(document).ready(function(){
-   $('.dropdown-submenu a.test').on("click", function(e){
-     $(this).next('ul').toggle();
-     e.stopPropagation();
-    e.preventDefault();
-   });
- });
+// by removing this code and using the custom css in the header has fixed this issue.
+ // $(document).ready(function(){
+ //   $('.dropdown-submenu a.test').on("click", function(e){
+ //     $(this).next('ul').toggle();
+ //     e.stopPropagation();
+ //    e.preventDefault();
+ //   });
+ // });
  </script>
  <!-- ====Nav Drop Down Submenu==== -->
- 
+
 </body>
 </html>
